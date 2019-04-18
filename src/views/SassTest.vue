0f54a7e700d26b350c9e8dd82ad483d530d23e44
Zhou Xuanzhuo <358409584@qq.com>
2019-04-18 17:04:07 +0800

    2-2. 群组选择器的嵌套;

--- src/views/SassTest.vue
+++ src/views/SassTest.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class='box'></div>
+  <div class="box">
+    <div id="child_1">child1</div>
+    <div id="child_2">child2</div>
+  </div>
 </template>
 
 <script>
@@ -14,19 +17,44 @@ export default {
   methods: {},
   components: {},
   created() {},
-  mounted() {},
+  mounted() {}
 };
 </script>
 
 <style lang="scss" scoped>
-.box{
-    width: 200px;
-    height: 200px;
-    border-width: 10px;
-    border-style: solid;
-    border-top-color: blue; 
+$highlight-color: rgba(180, 182, 47, 0.384);
+$aa: 300px;
+$bb: 100px;
+
+.box {
+  &:hover {
+    border-width: 1px;
+  }
+  width: $aa;
+  height: $aa;
+  border-width: 10px;
+  border-style: solid;
+  border-top-color: blue;
+  border-bottom-color: red;
+  border-left-color: $highlight-color;
+  border-right-color: green;
+  #child_1,
+  #child_2 {
+    width: $bb;
+    height: $bb;
+    border: 5px solid;
+  }
+  #child_1 {
+    border-top-color: blue;
     border-bottom-color: red;
-    border-left-color:yellow;
+    border-left-color: $highlight-color;
     border-right-color: green;
+  }
+  #child_2 {
+    border-top-color: red;
+    border-bottom-color: blue;
+    border-left-color: $highlight-color;
+    border-right-color: yellow;
+  }
 }
 </style>
