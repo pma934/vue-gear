102cf74a15566f8219f528becbb3b59fd7482f62
Zhou Xuanzhuo <358409584@qq.com>
2019-05-22 10:50:34 +0800

    mock 拦截 http请求测试

--- src/main.js
+++ src/main.js
@@ -6,10 +6,12 @@ import router from './router'
 import './registerServiceWorker'
 import axios from 'axios'
 import VueResource from 'vue-resource'
+import Mock from 'mockjs'
 
 Vue.use(VueResource)
 Vue.prototype.$axios = axios
-Vue.prototype.$Mock = require('mockjs')
+Vue.prototype.$Mock = Mock //require('mockjs')
+Mock.mock(/^http\:\/\/jsonplaceholder\.[\d\w\.\/]*$/, Mock.Random.dataImage('200x100', 'Hello Mock.js!'));
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,15 +1,43 @@
 <template>
-  <div>about
-    <div :class="{aa_a:1}">aaa</div>
+  <div>
+    about
+    <div :class="{aa_a:1}" @click="getHello">aaa</div>
+    <div v-html="mockData">{{mockData}}</div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  name: "About",
+  props: {},
+  data() {
+    return {
+      mockData: "<span style='color:red'>利用vue添加了插入一个span 元素</span>"
+    };
+  },
+  computed: {},
+  watch: {},
+  methods: {
+    getHello:function() {
+      this.$http.get("http://jsonplaceholder.typicode.com/comments").then(
+        response => {
+          this.mockData += "<image src="+ response.body +">";
+          console.log(response.body);
+        },
+        error => {
+          console.log(error);
+        }
+      );
+    }
+  },
+  components: {},
+  created() {},
+  mounted() {}
+};
 </script>
 
 <style scoped>
-.aa_a{
-  background-color: aqua
+.aa_a {
+  background-color: aqua;
 }
 </style>
